80317afde545a1813992c6bf34d7f5e94fe74f70
nraval1729 <nisarg_rawal@brown.edu>
2017-07-25 16:54:21 -0700

    can display random permuted word on front end

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/public/html');
 app.set('view engine', 'html');
 
 // To pick a random English word
-var randomWords = require('random-words');
+var randomWord = require('random-words');
 
 // To scrape the http://www.allscrabblewords.com/unscramble/ page
 var osmosis = require('osmosis');
@@ -19,8 +19,24 @@ var osmosis = require('osmosis');
 // Handlers
 app.get("/", function(req, res) {
 	console.log("Got to the home page!");
-	res.render("index.html");
+	var rw = randomWord();
+	res.render("index.html", {rw: shuffleString(rw)});
 });
+
+
+// Helper functions
+function shuffleString(s) {
+	var a = s.split(""),
+    n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
 
 app.listen(process.env.PORT || 5000);
 console.log("Server started. Listening.");
